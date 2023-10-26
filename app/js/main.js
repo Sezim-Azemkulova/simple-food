ad109613184de48d99400ae782c246b0da581d7e
Sezim <sezim.azemkulova.kk@gmail.com>
2023-10-27 01:16:15 +0700

    feat: add fixed header, finish header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 1) {
+    $(".header__top").addClass("fixed");
+  } else {
+    $(".header__top").removeClass("fixed");
+  }
+});
